702ff6e2c204cd61aa307b51da90c12a42ff800f
Rafale2000 <loick1morneau@icloud.com>
2024-12-29 15:14:34 +0100

    classe permettant de faire la liaisons entre les 2 pages

---
+++ src/main/java/org/example/r5a12/view/GestionPage.java
@@ -0,0 +1,11 @@
+package org.example.r5a12.view;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GestionPage extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
