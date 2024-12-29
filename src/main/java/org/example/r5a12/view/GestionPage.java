c7ca0fe468a52dc954d5e204779888e0e642821a
rafale2000 <loick1morneau@icloud.com>
2024-12-29 15:50:07 +0100

    classe qui permet de passer d'une page à l'autre

--- src/main/java/org/example/r5a12/view/GestionPage.java
+++ src/main/java/org/example/r5a12/view/GestionPage.java
@@ -7,5 +7,15 @@ public class GestionPage extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
+        PageParametre pageParametre = new PageParametre(stage);
+        PageAffichage pageAffichage = new PageAffichage(stage);
+
+        pageParametre.setNextScene(pageAffichage.getScene());
+        pageAffichage.setPreviousScene(pageParametre.getScene());
+
+        stage.setScene(pageParametre.getScene());
+        stage.setTitle("Application");
+        stage.show();
+
     }
 }
