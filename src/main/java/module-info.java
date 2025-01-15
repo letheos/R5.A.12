f4cbc71227c07bab7296229ecf9fdaa188704867
theo <102145780+letheos@users.noreply.github.com>
2025-01-15 16:11:08 +0100

    commit ne pas push sinon plus de git

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,8 +3,9 @@ module org.example.r5a12 {
     requires javafx.fxml;
     requires javafx.base;
     requires javafx.graphics;
-    requires javafx.charts;
+
     requires java.desktop;
+    requires com.google.gson;
 
     exports org.example.r5a12;
     opens org.example.r5a12 to javafx.fxml;
