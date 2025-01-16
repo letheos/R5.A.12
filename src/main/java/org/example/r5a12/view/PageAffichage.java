0dd98b25641ffd8b81b0b800d5c5320169c09f2a
Aymeric Morin <aymericmorin59@gmail.com>
2025-01-16 09:23:41 +0100

    Finalisation de Bezier

--- src/main/java/org/example/r5a12/view/PageAffichage.java
+++ src/main/java/org/example/r5a12/view/PageAffichage.java
@@ -2,7 +2,7 @@ package org.example.r5a12.view;
 
 import javafx.scene.Scene;
 import javafx.scene.chart.NumberAxis;
-import javafx.scene.chart.ScatterChart;
+import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
@@ -27,7 +27,7 @@ public class PageAffichage {
 
         final NumberAxis xAxis = new NumberAxis(0, 10, 1);
         final NumberAxis yAxis = new NumberAxis(0, 10, 1);
-        final ScatterChart<Number, Number> scatterChart = new ScatterChart<>(xAxis, yAxis);
+        final LineChart<Number, Number> LineChart = new LineChart<>(xAxis, yAxis);
 
         XYChart.Series<Number, Number> series1 = new XYChart.Series<>();
         ArrayList<Point> listePoints = new ArrayList<>();
@@ -35,8 +35,8 @@ public class PageAffichage {
         for (Point point : listePoints) {
             series1.getData().add(new XYChart.Data<>(point.getX(), point.getY()));
         }
-        scatterChart.getData().add(series1);
-        vboxPrincipale.getChildren().add(scatterChart);
+        LineChart.getData().add(series1);
+        vboxPrincipale.getChildren().add(LineChart);
 
         HBox hBoxButton = new HBox(10);
         Button saveButton = new Button("Sauvegarder");
